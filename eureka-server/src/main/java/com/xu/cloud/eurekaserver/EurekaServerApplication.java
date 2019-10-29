7477a7bcd82160b57da6b2a29ae5d5752b6671e8
“ge <1039753143@qq.com>
2019-10-29 15:38:19 +0800

    注册中心结束

--- eureka-server/src/main/java/com/xu/cloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/xu/cloud/eurekaserver/EurekaServerApplication.java
@@ -3,17 +3,25 @@ package com.xu.cloud.eurekaserver;
 import cn.hutool.core.util.NetUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * @author xuzhuang
+ */
 
 @SpringBootApplication
+@EnableEurekaServer // 表示它是个注册中心服务器
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
         int port = 8761;
-        if(NetUtil.isUsableLocalPort(port)){
+        if(!NetUtil.isUsableLocalPort(port)){
             System.err.printf("端口%d被占用", port);
             System.exit(1);
         }
-        SpringApplication.run(EurekaServerApplication.class, args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).properties("server.port=" + port).run(args);
     }
 
 }
